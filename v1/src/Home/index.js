666c600c87048f7776d4e25be4ec231f015b18d4
Ben Holmes <ben@coolship.io>
2018-08-09 17:14:30 +0000

    sets up local host devleopment env

--- v1/src/Home/index.js
+++ v1/src/Home/index.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
+import {NavLink} from "react-router-dom";
+
 class Home extends Component {
     static contextTypes = {
 	router: PropTypes.object
@@ -13,7 +15,7 @@ class Home extends Component {
   <div>
     <section id="welcome"><h1>WELCOME</h1><div className="biline">To <b>DNA-Microscopy.org</b>,
 	    the web platform for the sequencing-based imaging tool developed  in [REFERENCE].<p></p></div></section>
-    <section id="gallery"><h1>GALLERY</h1><div className="biline"><a onClick={()=>{this.context.router.history.push("/gallery");}}>Explore sample datasets</a></div></section>
+    <section id="gallery"><h1>GALLERY</h1><div className="biline"><NavLink to="/gallery">Explore sample datasets</NavLink></div></section>
     <section id="about"><h1>ABOUT</h1><div className="biline">The world's only all-sequencing imaging technology</div><div className="content"><p>Our technoogy uses a first of its kind methodology to infer spatial relationships between DNA sequences of transcripts in vitro. We use biophysical modeling and a novel strategy for high dimensional data embedding to create 2 dimensional maps of transcript sequences in biological datasets.</p><p>The output of our algorithm is a 2 dimensional image, with transcript sequences sequences mapped to individual pixels in a DNA microscopy image. Each pixel in the inferred image contains biological sequence data and can be used to infer complex spatial characteristics of gene expression on cellular and sub-cellular levels.</p></div></section>
 		<section id="contact"><h1>CONTACT</h1>
 		<div className="biline">info@dna-microscopy.org</div>
