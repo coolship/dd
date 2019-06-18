aea610c02c89f7d3551d81e73566e0b1a488e831
Ben Holmes <ben@coolship.io>
2019-06-18 16:25:58 -0400

    removes storage helpers

--- v1/src/config/firebase.js
+++ v1/src/config/firebase.js
@@ -5,10 +5,6 @@ import { FirebaseConfig } from "../config/keys";
 
 firebase.initializeApp(FirebaseConfig);
 
-
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require('firebase-functions');
-
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 admin.initializeApp()
