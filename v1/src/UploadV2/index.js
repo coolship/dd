af7b0b826a778844e498d3645636ad833848405a
Ben Holmes <ben@coolship.io>
2019-08-03 16:03:20 -0400

    improvements to viewing and uploading

--- v1/src/UploadV2/index.js
+++ v1/src/UploadV2/index.js
@@ -13,6 +13,7 @@ import { Route, Switch } from "react-router-dom";
 import XumiDropperContainer from "./XumiUploads";
 
 import Close from "react-icons/lib/md/close";
+import ProgressRing from "../widgets/ProgressRing"
 
 function loadingMessage(dataset) {
   const inprogress = _.pickBy(
@@ -258,41 +259,6 @@ class SimpleUploadView extends Component {
   }
 }
 
-class ProgressRing extends React.Component {
-  constructor(props) {
-    super(props);
-    const { radius, stroke } = this.props;
-    this.normalizedRadius = radius - stroke * 2;
-    this.circumference = this.normalizedRadius * 2 * Math.PI;
-  }
-
-  render() {
-
-    const { radius, stroke, progress } = this.props;
-    console.log(progress)
-    const strokeDashoffset =
-      this.circumference - (progress / 100) * this.circumference;
-    return (
-      <svg height={radius * 2} width={radius * 2}>
-        <circle
-          stroke="white"
-          fill="transparent"
-          strokeWidth={stroke}
-          strokeDasharray={this.circumference + " " + this.circumference}
-          style={{ strokeDashoffset }}
-          strokeWidth={stroke}
-          r={this.normalizedRadius}
-          cx={radius}
-          cy={radius}
-        />
-        <text x={radius } y={radius } className="small"
-        dominantBaseline="middle" textAnchor="middle">
-          {Math.round(progress)}%
-        </text>
-      </svg>
-    );
-  }
-}
 
 class UploadProgressView extends Component {
   constructor(props) {
